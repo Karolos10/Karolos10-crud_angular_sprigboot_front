e5bbaa35fec02be52c3abc3fbeb9938737061b0c
Carlos Miguel Rodriguez Botero <carlos.rodriguez1@utp.edu.co>
2024-08-09 16:55:09 -0500

    Creation of update methos, validation to know if is is updating or entering a new cliente, ,ethod of cleaning the form and methos of bringing the information in the form when a clients is going to be updated

--- src/app/components/card/card.component.ts
+++ src/app/components/card/card.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { cardModel } from '../../model/card-model';
 import { CardService } from '../../service/card.service';
+import { FormControl, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-card',
@@ -10,12 +11,22 @@ import { CardService } from '../../service/card.service';
 export class CardComponent implements OnInit{
 
   listCards: cardModel[] = [];
+  formCard: FormGroup = new FormGroup({});
+  isUpdate: boolean = false;
+
   constructor(private cardService: CardService){}
 
   ngOnInit(): void {
 
     this.list();
-      
+    this.formCard = new FormGroup({
+      id_card: new FormControl(''),
+      name: new FormControl(''),
+      number: new FormControl(''),
+      type: new FormControl(''),
+      cvv: new FormControl(''),
+      status: new FormControl('1')
+    });    
   }
 
   list(){
@@ -24,6 +35,20 @@ export class CardComponent implements OnInit{
         this.listCards = resp;
       }
     });
+  }
+
+  newCard(){
+    this.isUpdate = false;
+    this.formCard.reset();
+  }
+
+  selectItem(item: any){
+    this.isUpdate = true;
+    this.formCard.controls['id_card'].setValue(item.id_card);
+    this.formCard.controls['name'].setValue(item.name);
+    this.formCard.controls['number'].setValue(item.number);
+    this.formCard.controls['type'].setValue(item.type);
+    this.formCard.controls['cvv'].setValue(item.cvv);
   }
 
 }
