d90f455804325742a0b4e19b58f9ecb567b88936
Carlos Miguel Rodriguez Botero <carlos.rodriguez1@utp.edu.co>
2024-08-09 16:34:06 -0500

    creation of service layer and connection to the backend, creation of list method

--- src/app/components/card/card.component.ts
+++ src/app/components/card/card.component.ts
@@ -1,10 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { cardModel } from '../../model/card-model';
+import { CardService } from '../../service/card.service';
 
 @Component({
   selector: 'app-card',
   templateUrl: './card.component.html',
   styleUrl: './card.component.sass'
 })
-export class CardComponent {
+export class CardComponent implements OnInit{
+
+  listCards: cardModel[] = [];
+  constructor(private cardService: CardService){}
+
+  ngOnInit(): void {
+
+    this.list();
+      
+  }
+
+  list(){
+    this.cardService.getCards().subscribe( resp => {
+      if(resp){
+        this.listCards = resp;
+      }
+    });
+  }
 
 }
--- src/app/service/card.service.ts
+++ src/app/service/card.service.ts
@@ -1,9 +1,33 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { map, Observable } from 'rxjs';
+import { cardModel } from '../model/card-model';
+import { environment } from '../../environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class CardService {
 
-  constructor() { }
+  URL_API: string = environment.API_CARD;
+
+  constructor(private httpClient: HttpClient) {
+
+  }
+
+  getCards(): Observable<cardModel []> {
+    return this.httpClient.get<cardModel[]>(this.URL_API + '/list').pipe(map(res => res));
+  }
+
+  saveCard(request: any): Observable<any> {
+    return this.httpClient.post<any>(this.URL_API + '/save', request).pipe(map(res => res));
+  }
+
+  updateCard(request: any): Observable<any> {
+    return this.httpClient.post<any>(this.URL_API + '/update', request).pipe(map(res => res));
+  }
+
+  deleteCard(id: number): Observable<any> {
+    return this.httpClient.get<any>(this.URL_API + '/delete' + id).pipe(map(res => res));
+  }
 }
